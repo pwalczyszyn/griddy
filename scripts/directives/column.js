8841c5a07e88a29049bd6671f812a4ee7001c442
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2014-01-13 22:11:11 +0100

    implemented handling offsets

--- scripts/directives/column.js
+++ scripts/directives/column.js
@@ -9,7 +9,7 @@ angular.module('griddy.directives').directive('column', function () {
         replace: true,
         link: function ($scope, element, attrs) {
 
-            $scope.expandColumn = function () {
+            function findClass(regex, callback) {
                 var classList = element[0].classList,
                     columnClass,
                     columnNum;
@@ -17,44 +17,58 @@ angular.module('griddy.directives').directive('column', function () {
                 for (var i = 0, len = classList.length, c; i < len; i++) {
                     c = classList[i];
 
-                    if (/gd-column-[1-9][0-2]?/.test(c)) {
+                    if (regex.test(c)) {
                         columnClass = c;
                         columnNum = Number(columnClass.substr(columnClass.lastIndexOf('-') + 1));
                         break;
                     }
                 }
 
-                columnNum++;
-                if (columnNum <= 12) {
-                    classList.remove(columnClass);
-                    columnClass = 'gd-column-' + columnNum;
-                    classList.add(columnClass);
-                }
+                callback(columnClass, columnNum);
+            }
+
+            $scope.expandColumn = function () {
+                findClass(/gd-column-[1-9][0-2]?/, function (columnClass, columnNum) {
+                    columnNum++;
+                    if (columnNum <= 12) {
+                        element.removeClass(columnClass).addClass('gd-column-' + columnNum);
+                    }
+                });
             };
 
             $scope.collapseColumn = function () {
-                var classList = element[0].classList,
-                    columnClass,
-                    columnNum;
-
-                for (var i = 0, len = classList.length, c; i < len; i++) {
-                    c = classList[i];
-
-                    if (/gd-column-[1-9][0-2]?/.test(c)) {
-                        columnClass = c;
-                        columnNum = Number(columnClass.substr(columnClass.lastIndexOf('-') + 1));
-                        break;
+                findClass(/gd-column-[1-9][0-2]?/, function (columnClass, columnNum) {
+                    columnNum--;
+                    if (columnNum >= 1) {
+                        element.removeClass(columnClass).addClass('gd-column-' + columnNum);
                     }
-                }
-
-                columnNum--;
-                if (columnNum >= 1) {
-                    classList.remove(columnClass);
-                    columnClass = 'gd-column-' + columnNum;
-                    classList.add(columnClass);
-                }
+                });
             };
 
+            $scope.addOffset = function () {
+                findClass(/gd-offset-[1-9][0-1]?/, function (columnClass, columnNum) {
+                    if (columnClass === undefined) {
+                        element.addClass('gd-offset-1');
+                    } else {
+                        columnNum++;
+                        if (columnNum <= 11) {
+                            element.removeClass(columnClass).addClass('gd-offset-' + columnNum);
+                        }
+                    }
+                });
+            };
+
+            $scope.removeOffset = function () {
+                findClass(/gd-offset-[1-9][0-1]?/, function (columnClass, columnNum) {
+                    if (columnClass !== undefined) {
+                        columnNum--;
+                        element.removeClass(columnClass);
+                        if (columnNum > 0) {
+                            element.addClass('gd-offset-' + columnNum);
+                        }
+                    }
+                });
+            };
 
         }
     };
